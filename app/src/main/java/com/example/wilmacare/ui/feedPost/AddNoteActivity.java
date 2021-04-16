8e16afe1b7d908b79f1d4cea860956b0f8c60054
Anne Lindberg <anne.lindberg@ambiant.io>
2021-04-16 07:51:37 +0200

    Added the AddNoteActivity but the onClick does not work yet

--- app/src/main/java/com/example/wilmacare/ui/feedPost/AddNoteActivity.java
+++ app/src/main/java/com/example/wilmacare/ui/feedPost/AddNoteActivity.java
@@ -1,16 +1,70 @@
 package com.example.wilmacare.ui.feedPost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.wilmacare.R;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+    public static final String EXTRA_USERNAME = "com.example.wilmacare.ui.feedPost.EXTRA_USERNAME";
+    public static final String EXTRA_DESCRIPTION = "com.example.wilmacare.ui.feedPost.EXTRA_DESCRIPTION";
+
+    private EditText editTextUsername;
+    private EditText editTextDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        editTextUsername = findViewById(R.id.edt_NameOfPerson);
+        editTextDescription = findViewById(R.id.edt_description);
+
+        setTitle("Add post");
+    }
+
+    private void savePost() {
+        String username = editTextUsername.getText().toString();
+        String description = editTextDescription.getText().toString();
+
+        if (username.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this, "Skriv venligst noget i alle felter", Toast.LENGTH_SHORT);
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_USERNAME, username);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_post:
+                savePost();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/wilmacare/ui/feedPost/FeedPostFragment.java
+++ app/src/main/java/com/example/wilmacare/ui/feedPost/FeedPostFragment.java
@@ -1,42 +1,50 @@
 package com.example.wilmacare.ui.feedPost;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wilmacare.MainActivity;
 import com.example.wilmacare.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
+import static android.app.Activity.RESULT_OK;
+
 public class FeedPostFragment extends Fragment {
 
     private FeedPostViewModel feedPostViewModel;
     private RecyclerView recyclerView;
-    private FloatingActionButton floatingActionButton;
-
+    private Button buttonAddPost;
+    public static final int ADD_POST_REQUEST = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_feedpost, container, false);
         feedPostViewModel = new ViewModelProvider(this).get(FeedPostViewModel.class);
 
-        floatingActionButton = root.findViewById(R.id.fabAddFeedPost);
+
         View view = inflater.inflate(R.layout.fragment_feedpost, container, false);
 
 
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.setHasFixedSize(true);
-
 
 
         final FeedPostAdapter adapter = new FeedPostAdapter();
@@ -52,8 +60,36 @@ public class FeedPostFragment extends Fragment {
             }
         });
 
+        buttonAddPost = root.findViewById(R.id.btnAddFeedPost);
+
+        buttonAddPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("Action button clicked", "");
+                Intent intent = new Intent(getActivity(), AddNoteActivity.class);
+                startActivityForResult(intent, ADD_POST_REQUEST);
+            }
+        });
+
         return view;
-
-
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_POST_REQUEST && resultCode == RESULT_OK) {
+            String username = data.getStringExtra(AddNoteActivity.EXTRA_USERNAME);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+
+            FeedPost feedPost = new FeedPost(username, description);
+            feedPostViewModel.insert(feedPost);
+
+        } else {
+            //Toast.makeText(this,"Opslag ikke gemt", Toast.LENGTH_SHORT).show();
+            Log.v("Opslag ikke gemt", "");
+            //TODO: hvorfor virker toasten ikke?
+        }
+    }
+
 }
